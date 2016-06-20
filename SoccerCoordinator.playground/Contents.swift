61b2646373ec11a2f2aa8c7f2133bf174ed64d92
Christopher Bonuel <bonuelc@gmail.com>
2016-06-20 15:45:30 -0600

    Add playground file with player dictionary

---
+++ SoccerCoordinator.playground/Contents.swift
@@ -0,0 +1,132 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let joeSmithDict: Dictionary = [
+  "height": 42,
+  "soccer experience": true,
+  "guardians": "Jim and Jan Smith"
+]
+
+let jillTannerDict: Dictionary = [
+  "height": 36,
+  "soccer experience": true,
+  "guardians": "Clara Tanner"
+]
+
+let billBonDict: Dictionary = [
+  "height": 43,
+  "soccer experience": true,
+  "guardians": "Sara and Jenny Bon"
+]
+
+let evaGordonDict: Dictionary = [
+  "height": 45,
+  "soccer experience": false,
+  "guardians": "Wendy and Mike Gordon"
+]
+
+let mattGillDict: Dictionary = [
+  "height": 40,
+  "soccer experience": false,
+  "guardians": "Charles and Sylvia Gill"
+]
+
+let kimmySteinDict: Dictionary = [
+  "height": 41,
+  "soccer experience": false,
+  "guardians": "Bill and Hillary Stein"
+]
+
+let sammyAdamsDict: Dictionary = [
+  "height": 45,
+  "soccer experience": false,
+  "guardians": "Jeff Adams" ]
+]
+
+let karlSayganDict: Dictionary = [
+  "height": 42,
+  "soccer experience": true,
+  "guardians": "Heather Heather Bledsoe"
+]
+
+let suzaneGreenbergiDict: Dictionary = [
+  "height": 44,
+  "soccer experience": true,
+  "guardians": "Henrietta Dumas"
+]
+
+let salDaliDict: Dictionary = [
+  "height": 41,
+  "soccer experience": false,
+  "guardians": "Gala Dali"
+]
+
+let joeKavalierDict: Dictionary = [
+  "height": 39,
+  "soccer experience": false,
+  "guardians": "Gala Dali"
+]
+
+let benFinkelsteinDict: Dictionary = [
+  "height": 44,
+  "soccer experience": false,
+  "guardians": "Aaron and Jill Finkelstein"
+]
+
+let diegoSotoDict: Dictionary = [
+  "height": 41,
+  "soccer experience": true,
+  "guardians": "Robin and Sarika Soto"
+]
+
+let chloeAlaskaDict: Dictionary = [
+  "height": 47,
+  "soccer experience": false,
+  "guardians": "David and Jamie Alaska"
+]
+
+let arnoldWillisDict: Dictionary = [
+  "height": 43,
+  "soccer experience": false,
+  "guardians": "Claire Willis"
+]
+
+let phillipHelmDict: Dictionary = [
+  "height": 44,
+  "soccer experience": true,
+  "guardians": "Thomas Helm and Eva Jones"
+]
+
+let lesClayDict: Dictionary = [
+  "height": 42,
+  "soccer experience": true,
+  "guardians": "Wynonna Brown"
+]
+
+let herschelKrustofskiDict: Dictionary = [
+  "height": 45,
+  "soccer experience": true,
+  "guardians": "Kyman and Rachel Krustofski"
+]
+
+let playersDict: Dictionary = [
+  "Joe Smith": joeSmithDict,
+  "Jill Tanner": jillTannerDict,
+  "Bill Bon": billBonDict,
+  "Eva Gordon": evaGordonDict,
+  "Matt Gill": mattGillDict,
+  "Kimmy Stein": kimmySteinDict,
+  "Sammy Adams": sammyAdamsDict,
+  "Karl Saygan": karlSayganDict,
+  "Suzane Greenberg": suzaneGreenbergiDict,
+  "Sal Dali": salDaliDict,
+  "Joe Kavalier": joeKavalierDict,
+  "Ben Finkelstein": benFinkelsteinDict,
+  "Diego Soto": diegoSotoDict,
+  "Chloe Alaska": chloeAlaskaDict,
+  "Arnold Willis": arnoldWillisDict,
+  "Phillip Helm": phillipHelmDict,
+  "Les Clay": lesClayDict,
+  "Herschel Krustofski": herschelKrustofskiDict
+]
