fd7ca4e7b9b42ca9121b563a204c552a350f1bc5
Christopher Bonuel <bonuelc@gmail.com>
2016-06-21 05:32:43 -0600

    final version

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -2,131 +2,213 @@
 
 import UIKit
 
-let joeSmithDict: Dictionary = [
-  "height": 42,
-  "soccer experience": true,
-  "guardians": "Jim and Jan Smith"
+// Constants for dictionary keys
+let name: String = "name"
+let height: String = "height"
+let soccerExperience: String = "soccer experience"
+let guardians: String = "guardians"
+
+let joeSmith: Dictionary<String, Any> = [
+    name: "Joe Smith",
+    height: 42,
+    soccerExperience: true,
+    guardians: "Jim and Jan Smith"
 ]
 
-let jillTannerDict: Dictionary = [
-  "height": 36,
-  "soccer experience": true,
-  "guardians": "Clara Tanner"
+let jillTanner: Dictionary<String, Any> = [
+    name: "Jill Tanner",
+    height: 36,
+    soccerExperience: true,
+    guardians: "Clara Tanner"
 ]
 
-let billBonDict: Dictionary = [
-  "height": 43,
-  "soccer experience": true,
-  "guardians": "Sara and Jenny Bon"
+let billBon: Dictionary<String, Any> = [
+    name: "Bill Bon",
+    height: 43,
+    soccerExperience: true,
+    guardians: "Sara and Jenny Bon"
 ]
 
-let evaGordonDict: Dictionary = [
-  "height": 45,
-  "soccer experience": false,
-  "guardians": "Wendy and Mike Gordon"
+let evaGordon: Dictionary<String, Any> = [
+    name: "Eva Gordon",
+    height: 45,
+    soccerExperience: false,
+    guardians: "Wendy and Mike Gordon"
 ]
 
-let mattGillDict: Dictionary = [
-  "height": 40,
-  "soccer experience": false,
-  "guardians": "Charles and Sylvia Gill"
+let mattGill: Dictionary<String, Any> = [
+    name: "Matt Gill",
+    height: 40,
+    soccerExperience: false,
+    guardians: "Charles and Sylvia Gill"
 ]
 
-let kimmySteinDict: Dictionary = [
-  "height": 41,
-  "soccer experience": false,
-  "guardians": "Bill and Hillary Stein"
+let kimmyStein: Dictionary<String, Any> = [
+    name: "Kimmy Stein",
+    height: 41,
+    soccerExperience: false,
+    guardians: "Bill and Hillary Stein"
 ]
 
-let sammyAdamsDict: Dictionary = [
-  "height": 45,
-  "soccer experience": false,
-  "guardians": "Jeff Adams" ]
+let sammyAdams: Dictionary<String, Any> = [
+    name: "Sammy Adams",
+    height: 45,
+    soccerExperience: false,
+    guardians: "Jeff Adams"
 ]
 
-let karlSayganDict: Dictionary = [
-  "height": 42,
-  "soccer experience": true,
-  "guardians": "Heather Heather Bledsoe"
+let karlSaygan: Dictionary<String, Any> = [
+    name: "Karl Saygan",
+    height: 42,
+    soccerExperience: true,
+    guardians: "Heather Bledsoe"
 ]
 
-let suzaneGreenbergiDict: Dictionary = [
-  "height": 44,
-  "soccer experience": true,
-  "guardians": "Henrietta Dumas"
+let suzaneGreenbergi: Dictionary<String, Any> = [
+    name: "Suzane Greenberg",
+    height: 44,
+    soccerExperience: true,
+    guardians: "Henrietta Dumas"
 ]
 
-let salDaliDict: Dictionary = [
-  "height": 41,
-  "soccer experience": false,
-  "guardians": "Gala Dali"
+let salDali: Dictionary<String, Any> = [
+    name: "Sal Dali",
+    height: 41,
+    soccerExperience: false,
+    guardians: "Gala Dali"
 ]
 
-let joeKavalierDict: Dictionary = [
-  "height": 39,
-  "soccer experience": false,
-  "guardians": "Gala Dali"
+let joeKavalier: Dictionary<String, Any> = [
+    name: "Joe Kavalier",
+    height: 39,
+    soccerExperience: false,
+    guardians: "Sam and Elaine Kavalier"
 ]
 
-let benFinkelsteinDict: Dictionary = [
-  "height": 44,
-  "soccer experience": false,
-  "guardians": "Aaron and Jill Finkelstein"
+let benFinkelstein: Dictionary<String, Any> = [
+    name: "Ben Finkelstein",
+    height: 44,
+    soccerExperience: false,
+    guardians: "Aaron and Jill Finkelstein"
 ]
 
-let diegoSotoDict: Dictionary = [
-  "height": 41,
-  "soccer experience": true,
-  "guardians": "Robin and Sarika Soto"
+let diegoSoto: Dictionary<String, Any> = [
+    name: "Diego Soto",
+    height: 41,
+    soccerExperience: true,
+    guardians: "Robin and Sarika Soto"
 ]
 
-let chloeAlaskaDict: Dictionary = [
-  "height": 47,
-  "soccer experience": false,
-  "guardians": "David and Jamie Alaska"
+let chloeAlaska: Dictionary<String, Any> = [
+    name: "Chloe Alaska",
+    height: 47,
+    soccerExperience: false,
+    guardians: "David and Jamie Alaska"
 ]
 
-let arnoldWillisDict: Dictionary = [
-  "height": 43,
-  "soccer experience": false,
-  "guardians": "Claire Willis"
+let arnoldWillis: Dictionary<String, Any> = [
+    name: "Arnold Willis",
+    height: 43,
+    soccerExperience: false,
+    guardians: "Claire Willis"
 ]
 
-let phillipHelmDict: Dictionary = [
-  "height": 44,
-  "soccer experience": true,
-  "guardians": "Thomas Helm and Eva Jones"
+let phillipHelm: Dictionary<String, Any> = [
+    name: "Phillip Helm",
+    height: 44,
+    soccerExperience: true,
+    guardians: "Thomas Helm and Eva Jones"
 ]
 
-let lesClayDict: Dictionary = [
-  "height": 42,
-  "soccer experience": true,
-  "guardians": "Wynonna Brown"
+let lesClay: Dictionary<String, Any> = [
+    name: "Les Clay",
+    height: 42,
+    soccerExperience: true,
+    guardians: "Wynonna Brown"
 ]
 
-let herschelKrustofskiDict: Dictionary = [
-  "height": 45,
-  "soccer experience": true,
-  "guardians": "Kyman and Rachel Krustofski"
+let herschelKrustofski: Dictionary<String, Any> = [
+    name: "Herschel Krustofski",
+    height: 45,
+    soccerExperience: true,
+    guardians: "Kyman and Rachel Krustofski"
 ]
 
-let playersDict: Dictionary = [
-  "Joe Smith": joeSmithDict,
-  "Jill Tanner": jillTannerDict,
-  "Bill Bon": billBonDict,
-  "Eva Gordon": evaGordonDict,
-  "Matt Gill": mattGillDict,
-  "Kimmy Stein": kimmySteinDict,
-  "Sammy Adams": sammyAdamsDict,
-  "Karl Saygan": karlSayganDict,
-  "Suzane Greenberg": suzaneGreenbergiDict,
-  "Sal Dali": salDaliDict,
-  "Joe Kavalier": joeKavalierDict,
-  "Ben Finkelstein": benFinkelsteinDict,
-  "Diego Soto": diegoSotoDict,
-  "Chloe Alaska": chloeAlaskaDict,
-  "Arnold Willis": arnoldWillisDict,
-  "Phillip Helm": phillipHelmDict,
-  "Les Clay": lesClayDict,
-  "Herschel Krustofski": herschelKrustofskiDict
+let players: [Dictionary<String, Any>] = [
+    joeSmith,
+    jillTanner,
+    billBon,
+    evaGordon,
+    mattGill,
+    kimmyStein,
+    sammyAdams,
+    karlSaygan,
+    suzaneGreenbergi,
+    salDali,
+    joeKavalier,
+    benFinkelstein,
+    diegoSoto,
+    chloeAlaska,
+    arnoldWillis,
+    phillipHelm,
+    lesClay,
+    herschelKrustofski
 ]
+
+// Separate players into arrays by experience
+var experiencedPlayers: [Dictionary<String, Any>] = []
+var inexperiencedPlayers: [Dictionary<String, Any>] = []
+for player in players {
+    if player[soccerExperience] as! Bool == true {
+        experiencedPlayers.append(player)
+    } else {
+        inexperiencedPlayers.append(player)
+    }
+}
+
+// Initialize teams
+var sharks: [Dictionary<String, Any>] = []
+var dragons: [Dictionary<String, Any>] = []
+var raptors: [Dictionary<String, Any>] = []
+
+// Divvy experienced players between teams
+for i in 0..<experiencedPlayers.count {
+    let player = experiencedPlayers[i]
+    let mod = i % 3
+    if mod == 0 {
+        sharks.append(player)
+    } else if mod == 1 {
+        dragons.append(player)
+    } else {
+        raptors.append(player)
+    }
+}
+
+// Similarly divvy inexperienced players between teams
+// Divvy them in backwards order to mitigate the possibility of uneven number of players
+for i in 0..<inexperiencedPlayers.count {
+    let player = inexperiencedPlayers[i]
+    let mod = i % 3
+    if mod == 0 {
+        raptors.append(player)
+    } else if mod == 1 {
+        dragons.append(player)
+    } else {
+        sharks.append(player)
+    }
+}
+
+// Print Sharks letters
+for player in sharks {
+    print("Dear \(player[guardians]), your child, \(player[name]), has been placed on the team Sharks. The first practice for the Sharks is March 17, 3pm.")
+}
+
+// Print Dragons letters
+for player in dragons {
+  print("Dear \(player[guardians]), your child, \(player[name]), has been placed on the team Dragons. The first practice for the Dragons is March 17, 1pm.")
+}
+
+// Print Raptors letters
+for player in raptors {
+  print("Dear \(player[guardians]), your child, \(player[name]), has been placed on the team Raptors. The first practice for the Raptors is March 18, 1pm.")
+}
